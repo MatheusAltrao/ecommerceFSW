8bc5988671d768fdb303c90200a3e9b28f8b213d
Matheus Altrão <matheusaltrao2@gmail.com>
2023-10-19 23:35:01 -0400

    feat: add second banner i home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,11 +29,21 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
-        <h2 className="uppercase">Ofertas</h2>
+        <h2 className="font-semibold uppercase">Ofertas</h2>
         <div className="mb-8 mt-5">
           <ProductList products={deals} />
         </div>
       </div>
+
+      <Image
+        src="/images/banner-02.png"
+        width={0}
+        height={0}
+        className="h-auto w-full object-cover"
+        sizes="100vw"
+        alt="55% de desconto em mouses!"
+        priority={true}
+      />
     </div>
   );
 }
